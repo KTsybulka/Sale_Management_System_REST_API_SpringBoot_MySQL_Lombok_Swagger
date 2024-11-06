b361d62f221569a05d25e5eba9e54dc3a3087316
KTsybulka <ktsybulka11@gmail.com>
2024-11-06 12:04:05 -0500

    Initial commit

---
+++ src/main/java/com/example/n01561393_Assignment05_/Sale_management_System/N01561393Assignment05SaleManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.n01561393_Assignment05_.Sale_management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N01561393Assignment05SaleManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N01561393Assignment05SaleManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/n01561393_Assignment05_/Sale_management_System/N01561393Assignment05SaleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.n01561393_Assignment05_.Sale_management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N01561393Assignment05SaleManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
